87a25dd502d42beb2d7bca6a8a4dcfb94f03dd7a
Macarena Carreno <macarenacarreno@Macarenas-MacBook-Pro.local>
2019-06-18 12:02:47 -0400

    adding azure authentication with AOUTH

---
+++ server/auth/azure.js
@@ -0,0 +1,50 @@
+const passport = require('passport')
+const router = require('express').Router()
+const AzureAdOAuth2Strategy = require('passport-azure-oauth').OAuth2Strategy
+const {User} = require('../db/models')
+module.exports = router
+
+if (!process.env.AZURE_CLIENT_ID || !process.env.AZURE_CLIENT_SECRET) {
+  console.log('Azure client ID / secret not found. Skipping Azure OAuth.')
+} else {
+  const azureConfig = {
+    clientID: process.env.AZURE_CLIENT_ID,
+    clientSecret: process.env.AZURE_CLIENT_SECRET,
+    callbackURL: process.env.AZURE_CALLBACK,
+    resource: '00000002-0000-0000-c000-000000000000',
+    tenant: 'plandigital.onmicrosoft.com'
+  }
+
+  const strategy = new AzureAdOAuth2Strategy(azureConfig, function(
+    accessToken,
+    refresh_token,
+    params,
+    profile,
+    done
+  ) {
+    // currently we can't find a way to exchange access token by user info (see userProfile implementation), so
+    // you will need a jwt-package like https://github.com/auth0/node-jsonwebtoken to decode id_token and get waad profile
+    var waadProfile = profile || jwt.decode(params.id_token)
+    console.log(waadProfile)
+
+    // this is just an example: here you would provide a model *User* with the function *findOrCreate*
+    User.findOrCreate({id: waadProfile.upn}, function(err, user) {
+      done(err, user)
+    })
+  })
+
+  passport.use(strategy)
+
+  router.get('/auth/azureadoauth2', passport.authenticate('azure_ad_oauth2'))
+
+  router.get(
+    '/auth/azureadoauth2/callback',
+    passport.authenticate('azure_ad_oauth2', {
+      failureRedirect: '/login'
+    }),
+    function(req, res) {
+      // Successful authentication, redirect home.
+      res.redirect('/')
+    }
+  )
+}
